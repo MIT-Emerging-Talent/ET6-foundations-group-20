68ac0ac580d3c80e8c057af75ab52d5644ebc626
matopcheg <supermatya03@gmail.com>
2025-01-03 10:15:35 +0200

    Minor naming changes

--- solutions/tests/is_palindrome.py
+++ solutions/tests/is_palindrome.py
@@ -6,16 +6,16 @@ class TestIsPalindrome(unittest.TestCase):
         self.solution = Solution()
 
     def test_positive_palindrome(self):
-        self.assertTrue(self.solution.isPalindrome(121))
+        self.assertTrue(self.solution.is_palindrome(121))
 
     def test_negative_number(self):
-        self.assertFalse(self.solution.isPalindrome(-121))
+        self.assertFalse(self.solution.is_palindrome(-121))
 
     def test_non_palindrome(self):
-        self.assertFalse(self.solution.isPalindrome(123))
+        self.assertFalse(self.solution.is_palindrome(123))
 
     def test_single_digit(self):
-        self.assertTrue(self.solution.isPalindrome(7))
+        self.assertTrue(self.solution.is_palindrome(7))
 
 if __name__ == '__main__':
     unittest.main()
